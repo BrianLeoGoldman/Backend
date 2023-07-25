f4ea1eee0c171165e4ca165509edecb5cd4a04cd
Brian Goldman <leonel89011@gmail.com>
2023-07-25 01:09:54 -0300

    Completed main.js file with app running

--- main.js
+++ main.js
@@ -1,62 +1,50 @@
-/* const http = require('http') // Es un objeto
+import { Contenedor } from './Desafios/desafio2.js'
+import { randomNumber } from './Utils/functions.js'
+
+// const express = require('express')
+import express from 'express' // This is because we added type: module in package.json
+
+const app = express()
 const PORT = 8080
+const contenedor = new Contenedor('products.txt')
+let visits = 0
 
-const server = http.createServer((req, res) => {
-    // req contiene datos del request realizado
-    // res tiene los datos que se enviaran al cliente
-    const date = new Date()
-    const hour = date.getHours()
-    let message = ''
-    if (hour >= 6 && hour < 12) {
-        message = 'Buenos dias'
-    }
-    else if (hour >= 12 && hour < 20) {
-        message = 'Buenas tardes'
-    }
-    else {
-        message = 'Buenas noches'
-    }
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end(message)
+app.get('/', (req, res) => {
+    visits++
+    res.send(`
+        <h1>Welcome to the site!</h1>
+        <h3>You are the visitor number ${ visits } to our site!</h3>
+        <h3>Choose an option:</h2>
+        <ul>
+            <li><a href="http://localhost:8080/productos">All our products</a></li>
+            <li><a href="http://localhost:8080/productoRandom">Any of a products</a></li>
+        </ul>
+    `)
 })
 
-const connectedServer = server.listen(PORT, () => {
-    console.log(`Servidor HTTP escuchando en el puerto ${connectedServer.address().port}`)
-}) */
-
-const express = require('express') // Es una funcion
-const app = express() // Es la aplicacion servidor que configuraremos
-const PORT = 8080
-
-let visitas = 0
-// Paths
-
-app.get("/", (req, res) => {
-    res.send("Mi primer endpoint")
+app.get('/productos', (req, res) => {
+    contenedor.getAll()
+        .then((response) => {
+            res.send(response)
+        })
+        .catch(error => {
+            res.send(error)
+        })
 })
 
-app.get("/visitas", (req, res) => {
-    const now = new Date()
-    const fyh = now.toLocaleString()
-    res.send(`La fecha y hora es ${fyh}`)
+app.get('/productoRandom', (req, res) => {
+    contenedor.getAll()
+        .then((response) => {
+            const randomNum = randomNumber(0, response.length - 1)
+            const random = response[randomNum]
+            res.send(random)
+        })
+        .catch(error => {
+            res.send(error)
+        })
 })
 
-app.get("/fyh", (req, res) => {
-    visitas++
-    res.send(`La cantidad de visitas es ${visitas}`)
-})
-
-app.get("/otra", (req, res) => {
-    res.send("Otra ruta")
-})
-
-app.get("*", (req, res) => {
-    res.send("Sin contenido")
-})
-
-// Levantamos el servidor en el puerto indicado
 const server = app.listen(PORT, () => {
-    console.log(`Servidor HTTP escuchando en el puerto ${server.address().port}`)
-}) 
-// Indicamos si hubo un error al establecer la conexion
-server.on("error", error => console.log(`Error en el servidor ${error}`))
+    console.log(`Server is running on port ${PORT}`)
+})
+server.on("error", (error) => console.log(`There was an error when starting the server: ${error}`))
