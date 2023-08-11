12fe7e68d5b1b1e142d26e2a09e9a41e6eb75b79
Brian Goldman <leonel89011@gmail.com>
2023-08-11 14:47:32 -0300

    Added http, handlebars and socket.io

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
 const express = require('express')
+const http = require('http')
 const path = require('path')
+const handlebars = require('express-handlebars')
+const socketIo = require('socket.io')
+const Swal = require('sweetalert2')
 const productsRouter = require("./routes/products.router.js")
 const cartRouter = require("./routes/carts.router.js")
 const app = express()
+const httpServer = http.createServer(app)
+const io = socketIo(httpServer)
 const PORT = 8080
+
+// HANDLEBARS
+app.engine('handlebars', handlebars.engine())
+
+// SERVER SETTINGS
+app.set('views', __dirname + '/views') 
+app.set('view engine', 'handlebars') 
 
 // MIDDLEWARES
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use("/", productsRouter)
 app.use("/", cartRouter)
 
